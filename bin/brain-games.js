1af838438ffe5614a47fc96afdcc49b88a1fcadf
orainmers <arnautvladislav5@gmail.com>
2024-04-12 21:47:12 +0300

    hello, userName

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import name from "../src/cli.js";
+
+console.log("Welcome to the Brain Games!");
+let userName = name;
+console.log(`Hello, ${name}`);
